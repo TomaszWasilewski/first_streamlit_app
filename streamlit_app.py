777176b55b4e75148a94141669ac578c9fbfa1ae
TomaszWasilewski <40766689+TomaszWasilewski@users.noreply.github.com>
2023-10-16 12:47:51 +0200

    Update streamlit_app.py
    remove emojis

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,14 +3,14 @@ import pandas
 import requests
 
 
-streamlit.title('My Parents New Healthy Dinner')
+streamlit.title('Healthy Dinner')
 streamlit.header('Breakfast Favorites')
-streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
-streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
-streamlit.text('🐔 Hard-Boiled Free-Range Egg')
-streamlit.text('🥑🍞 Avocado Toast')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.text('Avocado Toast')
 
-streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+streamlit.header('Build Your Own Fruit Smoothie')
 
 my_fruit_list = pandas.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
 my_fruit_list = my_fruit_list.set_index('Fruit')
